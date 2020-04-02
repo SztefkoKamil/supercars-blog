4d2458659bed1a4ac3d94d9d517f9658ed66131c
Kamil Sztefko <sztefkokamil@gmail.com>
2020-04-02 20:21:07 +0200

    Add Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar toggleable="md" fixed="top" type="dark" variant="dark">
+    <b-navbar-brand href="/">Supercars blog</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav class="justify-content-end">
+      <b-navbar-nav>
+        <b-nav-item href="/">Home</b-nav-item>
+        <b-nav-item href="posts">Posts</b-nav-item>
+        <b-nav-item href="about">About</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<style lang="css" scoped>
+.navbar a {
+  text-align: right;
+  font-size: 24px;
+  margin-right: 1rem;
+}
+</style>
