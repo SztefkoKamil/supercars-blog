1552228a19d9c883423a1e2c0604864244282965
Kamil Sztefko <sztefkokamil@gmail.com>
2020-04-02 21:06:53 +0200

    Add Carousel with dummy slides

---
+++ components/Carousel.vue
@@ -0,0 +1,42 @@
+<template>
+  <b-carousel
+    id="carousel-1"
+    :interval="3000"
+    background="#ababab"
+    img-width="1920"
+    img-height="1080"
+    style="text-shadow: 1px 1px 2px #333;"
+    @sliding-start="onSlideStart"
+    @sliding-end="onSlideEnd"
+  >
+    <b-carousel-slide
+      v-for="(slide, i) in slides"
+      :key="i"
+      :img-src="slide"
+    ></b-carousel-slide>
+  </b-carousel>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sliding: null,
+      slides: [
+        'https://picsum.photos/1024/576/?image=52',
+        'https://picsum.photos/1024/576/?image=54',
+        'https://picsum.photos/1024/576/?image=58',
+        'https://picsum.photos/1024/576/?image=55'
+      ]
+    }
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true
+    },
+    onSlideEnd(slide) {
+      this.sliding = false
+    }
+  }
+}
+</script>
