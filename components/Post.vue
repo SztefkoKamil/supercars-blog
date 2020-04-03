603451716577684406b8a013e2c1610e1b671ef3
Kamil Sztefko <sztefkokamil@gmail.com>
2020-04-03 20:54:34 +0200

    Add Post component

---
+++ components/Post.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-list-group-item class="border-0 mb-3">
+    <b-card
+      no-body
+      :img-src="postData.images[0]"
+      :img-alt="postData.Title"
+      img-top
+    >
+      <b-card-header>
+        <b-card-title>{{ postData.title }}</b-card-title>
+        <b-card-sub-title>{{ postData.date }}</b-card-sub-title>
+      </b-card-header>
+      <b-card-body>
+        <b-card-text
+          style="max-height:100px; height:min-content; overflow:hidden;"
+          >{{ postData.text[0] }}</b-card-text
+        >
+      </b-card-body>
+      <b-card-footer class="d-flex justify-content-between align-items-center">
+        <b-card-text class="m-0">{{ postData.author }}</b-card-text>
+        <b-button :href="`post/${postData.id}`" variant="info">Read</b-button>
+      </b-card-footer>
+    </b-card>
+  </b-list-group-item>
+</template>
+
+<script>
+export default {
+  props: {
+    postData: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
