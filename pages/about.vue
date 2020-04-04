f55e80cb5e44f6ace162557c3dfaafde68c8823c
Kamil Sztefko <sztefkokamil@gmail.com>
2020-04-04 16:21:15 +0200

    Add content to about page
    Added two supercars images.
    Added lorem text.

--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,65 @@
 <template>
-  <main>about</main>
+  <main class="container py-5">
+    <h1 class="text-center mb-4">Who we are?</h1>
+    <div class="row">
+      <div class="col col-12 col-lg-8">
+        <p class="mb-4 mb-md-5 mb-lg-0 px-3">
+          We are supercars fans propably just lakie you! Lorem ipsum dolor sit
+          amet, consectetur adipiscing elit. Phasellus sodales tristique arcu.
+          Cras maximus eleifend metus, eu posuere nibh sodales eu. Mauris sed
+          enim purus. Curabitur non odio non ante pulvinar dignissim. Aenean sed
+          tellus consectetur, pharetra quam eget, condimentum ligula. Nullam
+          semper quis ante nec dignissim. Aliquam a tincidunt enim. Maecenas vel
+          felis neque. Nam sit amet sapien lorem. Donec vitae egestas velit.
+          Aliquam erat volutpat. Sed vitae tempor felis. Nulla at posuere magna.
+          Nulla in ligula at ante dictum scelerisque. Mauris eu sapien ex.
+        </p>
+      </div>
+      <div class="col col-12 col-lg-4">
+        <img
+          class="w-100"
+          src="@/assets/images/Ferrari F8 Tributo-0.jpg"
+          alt=""
+        />
+      </div>
+    </div>
+    <p class="my-4 my-md-5 px-3">
+      Aliquam tristique blandit dignissim. Aliquam magna tortor, commodo nec
+      massa sit amet, blandit rutrum est. Maecenas finibus non velit nec
+      aliquet. Ut cursus ultricies porta. Sed fringilla varius lacinia. Maecenas
+      ultrices vestibulum quam, nec mollis mi. Donec augue augue, hendrerit quis
+      pharetra eu, mattis ac enim. Fusce vitae orci pellentesque, dignissim
+      lectus id, vehicula mi. Donec vitae egestas velit. Aliquam erat volutpat.
+      Sed vitae tempor felis. Nulla at posuere magna. Nulla in ligula at ante
+      dictum scelerisque. Mauris eu sapien ex.
+    </p>
+    <div class="row">
+      <div class="col col-12 col-lg-4">
+        <img
+          class="w-100"
+          src="@/assets/images/Pininfarina Battista-0.jpg"
+          alt=""
+        />
+      </div>
+      <div class="col col-12 col-lg-8">
+        <p class="mt-4 mt-md-5 mt-lg-0 px-3">
+          Nulla facilisi. Donec sit amet rutrum ante. Integer gravida diam sit
+          amet lorem pretium, in sollicitudin metus lacinia. Ut at tempus velit.
+          Curabitur sed risus nisi. In in elit vitae urna rutrum malesuada. In
+          sed placerat purus. Mauris in mollis est, vitae luctus diam. Praesent
+          venenatis, tortor in volutpat lacinia, justo dolor fringilla nulla,
+          porttitor suscipit arcu mi molestie nulla. Integer rutrum velit nec
+          mollis commodo. Integer eu magna diam. Sed sed est in nisi suscipit
+          fringilla at ac mauris. Nunc at semper nisi. Nunc at dolor gravida,
+          mattis ex at, tempor nunc. Suspendisse viverra urna odio, quis
+          tincidunt elit ullamcorper a.
+        </p>
+      </div>
+    </div>
+    <h2 class="text-center mt-5">Want to contact us?</h2>
+    <p class="text-center">
+      Fell free to send a message at
+      <a href="">contact@supercars-blog.com</a>
+    </p>
+  </main>
 </template>
