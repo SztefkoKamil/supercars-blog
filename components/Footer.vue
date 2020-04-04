18c878f82ae881b4253e62d65a028616e9b03032
Kamil Sztefko <sztefkokamil@gmail.com>
2020-04-04 15:16:15 +0200

    Add Footer component

---
+++ components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer
+    class="container-fluid d-flex justify-content-center py-5 bg-dark text-white"
+  >
+    <span>Created by Kamil Sztefko &copy; {{ new Date().getFullYear() }}</span>
+  </footer>
+</template>
