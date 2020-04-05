54cfb462cb711738fc2c3145bd8ff871a3b07e9b
Kamil Sztefko <sztefkokamil@gmail.com>
2020-04-05 20:02:53 +0200

    Prepare Post component for real data

--- components/Post.vue
+++ components/Post.vue
@@ -1,19 +1,14 @@
 <template>
   <b-list-group-item tag="li" class="border-0 mb-3">
-    <b-card
-      no-body
-      :img-src="postData.images[0]"
-      :img-alt="postData.Title"
-      img-top
-    >
+    <b-card no-body :img-src="imgURL" :img-alt="postData.Title" img-top>
       <b-card-header>
         <b-card-title>{{ postData.title }}</b-card-title>
-        <b-card-sub-title>{{ postData.date }}</b-card-sub-title>
+        <b-card-sub-title>{{ postData.data }}</b-card-sub-title>
       </b-card-header>
       <b-card-body>
         <b-card-text
           style="max-height:100px; height:min-content; overflow:hidden;"
-          >{{ postData.text[0] }}</b-card-text
+          >{{ content }}</b-card-text
         >
       </b-card-body>
       <b-card-footer class="d-flex justify-content-between align-items-center">
@@ -30,6 +25,33 @@ export default {
     postData: {
       type: Object,
       required: true
+    }
+  },
+  data() {
+    return {
+      imgURL: '',
+      content: ''
+    }
+  },
+  beforeMount() {
+    const img = this.returnFirstImage()
+    this.imgURL = process.env.BACKEND_URL + img.url
+    this.content = this.postData.content.split('\n\n')[0]
+  },
+  methods: {
+    returnFirstImage() {
+      const numbered = this.postData.images.map((img) => {
+        const dotIndex = img.name.lastIndexOf('.')
+        const extTrimed = img.name.slice(0, dotIndex)
+        const dashIndex = extTrimed.lastIndexOf('-')
+        img.nr = extTrimed.slice(dashIndex + 1)
+        return img
+      })
+
+      const sorted = numbered.sort((a, b) => {
+        return +a.nr - +b.nr
+      })
+      return sorted[0]
     }
   }
 }
