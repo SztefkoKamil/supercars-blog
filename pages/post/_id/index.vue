fc0f9bfec3bbb41cdab9d7f0f510bc1a2386d5e0
Kamil Sztefko <sztefkokamil@gmail.com>
2020-04-06 20:58:52 +0200

    Add real data fetch to post page
    Added fetching error info

--- pages/post/_id/index.vue
+++ pages/post/_id/index.vue
@@ -1,6 +1,23 @@
 <template>
   <main class="container py-5">
-    <b-card no-body :img-src="post.images[0]" :img-alt="post.Title" img-top>
+    <b-card v-if="fetchError">
+      <b-card-header>
+        <b-card-title title-tag="h3" class="text-danger">
+          <span class="d-block text-center"
+            >Sorry, we cannot fetch post from server.</span
+          >
+          <span class="d-block text-center mt-2">Some error occured.</span>
+          <span class="d-block text-center mt-2">Please try again later.</span>
+        </b-card-title>
+      </b-card-header>
+    </b-card>
+    <b-card
+      v-else
+      no-body
+      :img-src="post.images[0].url"
+      :img-alt="post.Title"
+      img-top
+    >
       <b-card-header>
         <b-card-title title-tag="h1" class="text-center">{{
           post.title
@@ -9,12 +26,12 @@
       <b-card-body>
         <div v-for="i in post.text.length" :key="i" class="mb-4">
           <b-card-text>{{ post.text[i - 1] }}</b-card-text>
-          <b-card-img :src="post.images[i]" :alt="post.title"></b-card-img>
+          <b-card-img :src="post.images[i].url" :alt="post.title"></b-card-img>
         </div>
         <b-card-img
-          v-for="(img, i) in restImages"
-          :key="i + img"
-          :src="img"
+          v-for="img in restImages"
+          :key="img.id"
+          :src="img.url"
           :alt="post.title"
           class="mb-4"
         ></b-card-img>
@@ -22,7 +39,7 @@
       <b-card-footer
         class="d-flex justify-content-between align-items-center py-4"
       >
-        <b-card-text class="m-0">{{ post.date }}</b-card-text>
+        <b-card-text class="m-0">{{ post.data }}</b-card-text>
         <b-card-text>{{ post.author }}</b-card-text>
       </b-card-footer>
     </b-card>
@@ -30,36 +47,61 @@
 </template>
 
 <script>
+const makeFullImagesPaths = (images) => {
+  const fullPathImages = images.map((img) => {
+    const fullPath = process.env.BACKEND_URL + img.url
+    img.url = fullPath
+    return img
+  })
+  return fullPathImages
+}
+
+const sortImages = (images) => {
+  const numbered = images.map((img) => {
+    const dotIndex = img.name.lastIndexOf('.')
+    const extTrimed = img.name.slice(0, dotIndex)
+    const dashIndex = extTrimed.lastIndexOf('-')
+    img.nr = extTrimed.slice(dashIndex + 1)
+    return img
+  })
+  const sorted = numbered.sort((a, b) => {
+    return +a.nr - +b.nr
+  })
+  return sorted
+}
+
+const divideContent = (content) => {
+  const dividedContent = content.split('\n\n')
+  return dividedContent
+}
+
+const preparePost = (rawPost) => {
+  const post = rawPost
+  const imagesWithPaths = makeFullImagesPaths(rawPost.images)
+  const sortedImages = sortImages(imagesWithPaths)
+  post.images = sortedImages
+  post.text = divideContent(post.content)
+  return post
+}
+
+const prepareRestImages = (images, textLength) => {
+  if (images.length <= textLength) return []
+  return images.slice(textLength + 1)
+}
+
 export default {
-  data() {
-    return {
-      post: {
-        id: 'gsedfgerfgdfgsdgf',
-        title: 'Lamborghini Aventador SVJ Roadster',
-        author: 'Stephan King',
-        date: '03-04-2020',
-        images: [
-          require('@/assets/images/Ferrari F8 Tributo-0.jpg'),
-          require('@/assets/images/Pininfarina Battista-0.jpg'),
-          require('@/assets/images/Ferrari F8 Tributo-0.jpg'),
-          require('@/assets/images/Pininfarina Battista-0.jpg'),
-          'https://i.picsum.photos/id/28/1024/576.jpg',
-          'https://i.picsum.photos/id/29/1024/576.jpg',
-          'https://i.picsum.photos/id/30/1024/576.jpg'
-        ],
-        text: [
-          'Lamborghini debuted its new Aventador SVJ Roadster at the Geneva Motor Show. While the car’s performance specs are more or less the same as the coupe version of the car, the convertible top gives it a look that’s truly its own. The removable roof features a two-piece design and is made of carbon fiber. Once removed, it fits securely in the frunk of the mid-engine vehicle.',
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus sodales tristique arcu. Cras maximus eleifend metus, eu posuere nibh sodales eu. Mauris sed enim purus. Curabitur non odio non ante pulvinar dignissim. Aenean sed tellus consectetur, pharetra quam eget, condimentum ligula. Nullam semper quis ante nec dignissim. Aliquam a tincidunt enim. Maecenas vel felis neque. Nam sit amet sapien lorem.',
-          'Aliquam tristique blandit dignissim. Aliquam magna tortor, commodo nec massa sit amet, blandit rutrum est. Maecenas finibus non velit nec aliquet. Ut cursus ultricies porta. Sed fringilla varius lacinia. Maecenas ultrices vestibulum quam, nec mollis mi. Donec augue augue, hendrerit quis pharetra eu, mattis ac enim. Fusce vitae orci pellentesque, dignissim lectus id, vehicula mi.'
-        ]
-      },
-      restImages: []
+  async asyncData({ params, app }) {
+    const url = process.env.BACKEND_URL + '/posts/' + params.id
+
+    try {
+      const response = await fetch(url)
+      const rawPost = await response.json()
+      const post = preparePost(rawPost)
+      const restImages = prepareRestImages(post.images, post.text.length)
+      return { post, restImages, fetchError: false }
+    } catch (err) {
+      return { fetchError: true }
     }
-  },
-  beforeMount() {
-    const images = [...this.post.images]
-    images.splice(0, this.post.text.length + 1)
-    this.restImages = images
   }
 }
 </script>
